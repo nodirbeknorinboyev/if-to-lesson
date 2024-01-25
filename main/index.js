37dbbead1955e162f4b96974d228d83c119ab11b
nodir2208 <norinboyevnortoy544@gmail.com>
2024-01-25 10:51:35 +0500

    uyga vazifa

---
+++ main/index.js
@@ -0,0 +1,52 @@
+//15-Masala
+//let a = Math.abs(5/3 - 13/4);
+//console.log(a);
+
+//18-Masala
+//let s;
+//let a = 6;
+//let b = 34;
+//s = a % 2 === 0 && b % 2 === 0;
+//console.log(s);
+
+//1-if-Masala
+//let a = 4;
+//let b = -9;
+//let c = 23;
+
+//if(a < b && a < c){
+ //   console.log(a);
+//} else if(b < a && b < c){
+ //   console.log(b);
+//}else{
+  //  console.log(c);
+//}
+
+//15-if-Masala
+
+//let x = -5;
+//let fx;
+//if (x > 0){
+//    fx = 2 * Math.sin(x);
+//}else{
+ //   fx = x - 6;
+//}
+//console.log(fx);
+
+//10-if-Masala
+let a = 3;
+let s;
+console.log(a % 2 == 1);
+if (a > 0 && a % 2 == 1){
+    s = "musbat toq son"
+}else if(a > 0 && a % 2 == 0){
+    s = "musbat juft son"
+}
+else if(a < 0 && a % 2 == 1){
+    s = "manfiy toq son"
+}else if(a < 0 && a % 2 == 0){
+    s = "manfiy juft son"
+}else{
+    s = "son 0 ga teng"
+}
+console.log(s);
